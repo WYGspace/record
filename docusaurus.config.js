cb530ff2f0b0b7fb74f9b751f6e66890be5f2560
WYGspace <1755650690@qq.com>
2022-03-25 18:41:08 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/WYGspace/record/find/main/',
+          editUrl: 'https://github.com/WYGspace/record/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/WYGspace/record/find/main/',
+          editUrl: 'https://github.com/WYGspace/record/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
