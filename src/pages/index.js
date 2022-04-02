e384c11b4019a5fb29c3d65c15e123198b276397
WYGspace <1755650690@qq.com>
2022-04-02 22:25:43 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,9 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      // title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
+      description="Description将进入元标签中 <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
